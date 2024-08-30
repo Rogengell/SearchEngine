346a2be6b6c8809505fe7383406ae2dc76617292
Jan_H_Christensen <janx802y@easv365.dk>
2024-08-30 19:36:57 +0200

    test

--- Indexer/Program.cs
+++ Indexer/Program.cs
@@ -7,25 +7,27 @@ namespace Indexer
 {
     class Program
     {
+        // test command:
         static void Main(string[] args)
         {
-            if(true){
+            if (true)
+            {
                 DecompressGzipFile("enron/mikro.tar.gz", "mails.tar");
-                if(Directory.Exists("maildir")) Directory.Delete("maildir", true);
-                
+                if (Directory.Exists("maildir")) Directory.Delete("maildir", true);
+
                 string binPath = AppDomain.CurrentDomain.BaseDirectory;
-                
+
                 if (binPath.EndsWith("bin\\Debug\\net7.0\\") || binPath.EndsWith("bin\\Release\\net7.0\\"))
                 {
-                    if(File.Exists(binPath+"._maildir")) File.Delete(binPath+"._maildir");
+                    if (File.Exists(binPath + "._maildir")) File.Delete(binPath + "._maildir");
                 }
                 TarFile.ExtractToDirectory("mails.tar", ".", false);
-                
+
                 new Renamer().Crawl(new DirectoryInfo("maildir"));
                 new App().Run();
             }
         }
-        
+
         static void DecompressGzipFile(string compressedFilePath, string decompressedFilePath)
         {
             using (FileStream compressedFileStream = File.OpenRead(compressedFilePath))
